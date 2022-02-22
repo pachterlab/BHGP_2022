e83a4c1609e806516ee007ab3a5009c9259c782d
Sina Booeshaghi <sbooeshaghi@gmail.com>
2022-02-21 16:12:54 -0800

    added plotting and utils functions

---
+++ scripts/plot.py
@@ -0,0 +1,184 @@
+from scipy import stats
+import numpy as np
+
+alpha = 0.5
+
+
+def plot_depth_norm(data, axs):
+    p = {
+        "xlabel": "Mean-cell count",
+        "ylabel": "Sum-cell count",
+        # "xscale": "symlog",
+        # "yscale": "symlog",
+    }
+
+    for idx, (ax, (title, matrix)) in enumerate(zip(axs, data.items())):
+        if idx > 0:
+            p.update({"ylabel": ""})
+        x = data["raw"].mean(1)
+        y = matrix.sum(1)
+        ax.scatter(x, y, edgecolor="k", facecolor="#D43F3A", alpha=alpha)
+
+        slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+
+        minx = min(x)
+        maxx = max(x)
+        x = np.linspace(minx, maxx, 10)
+        y = slope * x + intercept
+        ax.plot(
+            x,
+            y,
+            label=f"$r^2$ : {r_value**2:,.2f}\n$r$ : {r_value:,.2f}",
+            color="k",
+            linewidth=2,
+        )
+        ax.legend(fontsize=15)
+
+        p.update({"title": title})
+        ax.set(**p)
+
+    return axs
+
+
+def plot_depth_dist(data, axs):
+    p = {
+        "xlabel": "Cell counts",
+        "ylabel": "Frequency",
+    }
+
+    for idx, (title, matrix) in enumerate(data.items()):
+        ax = axs[idx]
+        if idx > 0:
+            p.update({"ylabel": ""})
+
+        x = np.sort(matrix.sum(1))[::-1]
+
+        mean = data["raw"].sum(1).mean()
+        stdev = np.sqrt(np.var(data["raw"].sum(1)))
+
+        close = np.all(np.allclose(x, x[0]))
+
+        if close:
+            ax.hist([x[0]] * len(x), edgecolor="k", facecolor="#D43F3A", bins=20)
+        else:
+            ax.hist(x, edgecolor="k", facecolor="#D43F3A", bins=20)
+        p.update({"title": title, "xlim": (mean - 1 * stdev, mean + 5 * stdev)})
+        ax.set(**p)
+    return axs
+
+
+def plot_knee(data, axs):
+    p = {
+        "xscale": "linear",
+        "yscale": "symlog",
+        "xlabel": "Cell counts",
+        "ylabel": "Cell rank",
+    }
+
+    for idx, (title, matrix) in enumerate(data.items()):
+        if idx > 0:
+            p.update({"ylabel": ""})
+        ax = axs[idx]
+
+        x = np.sort(matrix.sum(1))[::-1]
+        y = np.arange(x.shape[0])
+
+        ax.scatter(x, y, edgecolor="k", facecolor="#D43F3A", alpha=alpha)
+        p.update({"title": title})
+        ax.set(**p)
+    return axs
+
+
+def plot_pc_depth(data, pcs, axs):
+    p = {
+        "xlabel": "Summed-cell UMI counts",
+        "ylabel": "PC1",
+    }
+
+    for idx, (title, pc) in enumerate(pcs.items()):
+        if idx > 0:
+            p.update({"ylabel": ""})
+        ax = axs[idx]
+
+        # raw sumed counts for all
+        matrix = data["raw"]
+
+        x = matrix.sum(1)
+        y = pc[:, 0]
+        ax.scatter(x, y, edgecolor="k", facecolor="#D43F3A", alpha=alpha)
+
+        slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+
+        minx = min(x)
+        maxx = max(x)
+        x = np.linspace(minx, maxx, 10)
+        y = slope * x + intercept
+        ax.plot(
+            x,
+            y,
+            label=f"$r^2$ : {r_value**2:,.2f}\n$r$ : {r_value:,.2f}",
+            color="k",
+            linewidth=2,
+        )
+        ax.legend(fontsize=15)
+
+        p.update({"title": title})
+        ax.set(**p)
+    return axs
+
+
+def plot_mean_var(data, axs):
+    p = {
+        "xlabel": "Gene mean",
+        "ylabel": "Gene variance",
+        "xscale": "symlog",
+        "yscale": "symlog",
+        "ylim": (-1, 1e8),
+    }
+
+    for idx, (title, matrix) in enumerate(data.items()):
+        if idx > 0:
+            p.update({"ylabel": ""})
+        ax = axs[idx]
+
+        x = np.mean(matrix, axis=0)
+        y = np.var(matrix, axis=0)
+        ax.scatter(x, y, edgecolor="k", facecolor="#D43F3A", alpha=alpha)
+        p.update({"title": title})
+        ax.set(**p)
+    return axs
+
+
+def plot_monotone(data, rv, axs):
+
+    p = {"xlabel": "Spearman r", "ylabel": "Frequency"}
+
+    for idx, (label, m) in enumerate(data.items()):
+        if idx > 0:
+            p.update({"ylabel": ""})
+        ax = axs[idx]
+        x = rv[:, idx]
+
+        ax.hist(x, edgecolor="k", facecolor="#D43F3A")
+        p.update({"title": label})
+        ax.set(**p)
+    return axs
+
+
+def plot_example_gene(data, axs):
+    argmax = np.argmax(data["raw"].sum(0))
+    p = {
+        "xlabel": "Gene UMI counts",
+        "ylabel": "Frequency",
+    }
+
+    for idx, (title, matrix) in enumerate(data.items()):
+        if idx > 0:
+            p.update({"ylabel": ""})
+        ax = axs[idx]
+
+        g = matrix[:, argmax]
+        ax.hist(g, edgecolor="k", facecolor="#D43F3A")
+        p.update({"title": title})
+        ax.set(**p)
+    return axs
---
+++ scripts/utils.py
@@ -0,0 +1,60 @@
+from sklearn.preprocessing import normalize
+import numpy as np
+
+
+def read_str_list(fname, lst=list):
+    with open(fname, "r") as f:
+        for idx, line in enumerate(f.readlines()):
+            lst.append(line.strip())
+
+
+def sanitize_mtx(mtx: np.ndarray):
+    cell_count_mask = mtx.sum(1) > 0  # count for each cell
+    gene_count_mask = mtx.sum(0) > 0  # count for each gene
+
+    genes_detected_mask = (mtx > 0).sum(1) > 0  # n genes per cell
+    cells_detected_mask = (mtx > 0).sum(0) > 0  # n cells per gene
+    row_mask = np.logical_and(cell_count_mask, genes_detected_mask)
+    col_mask = np.logical_and(gene_count_mask, cells_detected_mask)
+
+    return (row_mask, col_mask)
+
+
+def do_pf(mtx, target_sum=None):
+    pf = mtx.sum(1).flatten()
+    mtx_pf = mtx / (pf / pf.mean())[:, None]
+    if target_sum:
+        mtx_pf = normalize(mtx, norm="l1") * target_sum
+    return mtx_pf
+
+
+def do_log1p_pf(mtx, iter=1):
+    print(f"iter: {iter}")
+    log1p = np.log1p(mtx)
+    pf = do_pf(log1p)
+
+    iter -= 1
+    if iter == 0:
+        return pf
+    pf_up = do_pf(np.exp(pf) - 1)
+    return do_log1p_pf(pf_up, iter)
+
+
+def norm(mtx):
+    d = {}
+    print("raw")
+    d["raw"] = mtx
+
+    print("pf")
+    d["pf"] = do_pf(mtx)
+
+    print("log1p")
+    d["log1p"] = np.log1p(mtx)
+
+    print("pf -> log1p")
+    d["pf -> log1p"] = np.log1p(do_pf(mtx))
+
+    print("pf -> log1p -> pf")
+    d["pf -> log1p -> pf"] = do_log1p_pf(do_pf(mtx))
+
+    return d
