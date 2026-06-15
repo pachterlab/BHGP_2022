a3dca2b9f2a342588adeba6e95fe867855dc15ba
Lior Pachter <lakigigar@gmail.com>
2026-06-14 21:24:33 -0700

    Tighten Figure 2c downsampling axis

--- scripts/plot_fig2abc_alpha_k_benchmark_style.R
+++ scripts/plot_fig2abc_alpha_k_benchmark_style.R
@@ -298,8 +298,8 @@ simulation_pl <- res_main %>%
 downsampling_pl <- res_main %>%
   filter(benchmark == "downsampling") %>%
   make_main_plot_panel(add_group_label = FALSE) +
-  coord_cartesian(xlim = c(0.2, 10)) +
-  scale_x_continuous(breaks = c(0.45, 1, 2, 5, 10), labels = c("0.5", "1", "2", "5", "10")) +
+  coord_cartesian(xlim = c(0.2, 6.2)) +
+  scale_x_continuous(breaks = c(0.45, 1, 2, 4, 6), labels = c("0.5", "1", "2", "4", "6")) +
   labs(
     x = "Relative $k$-NN overlap",
     subtitle = "Original versus downsampled deep-seq data"
